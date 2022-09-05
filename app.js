04fb0eb3d8bd734345342fb8fb13a8a6073786b5
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2022-09-05 19:49:58 -0400

    added AI mode  and reset game features to app

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const gameBoard = (() => {
   const boardBox = document.querySelectorAll('.box');
   const boardContainer = document.querySelector('.game-board');
   let board = [];
+  let gameOver = false;
+  let winner = null;
+
+  //allows us to add box to specific function so we can remove the exact event listener dynamically
+  const boxFunctions = [];
 
   //factory function for players
   function playerFactory(marker, turn) {
@@ -15,83 +20,101 @@ const gameBoard = (() => {
   const player1 = playerFactory('X', true);
   const player2 = playerFactory('O', false);
   const AI = playerFactory('O', false);
-  const whosTurn = document.querySelector('.turn-indicator');
-  let winner = null;
 
+  const whoseTurn = document.querySelector('.turn-indicator');
+
+  // ----------------------------PVP LOGIC--------------------------------------- //
   //allow players to place mark given it's their turn
-  const playerMark = function () {
-    boardBox.forEach((box) => {
-      box.addEventListener('click', () => {
+  function PVPLogic() {
+    for (let i = 0; i < boardBox.length; i++) {
+      boxFunctions[i] = function test() {
         //player 1 turn
-        if (box.innerText === '' && player1.turn === true && winner === null) {
-          whosTurn.innerText = 'Player 2 (O), make your move';
-          box.innerText = player1.marker;
+        if (
+          boardBox[i].innerText === '' &&
+          player1.turn === true &&
+          winner === null
+        ) {
+          whoseTurn.innerText = 'Player 2 (O), make your move';
+          boardBox[i].innerText = player1.marker;
           board.push(player1.marker);
           player1.turn = false;
           player2.turn = true;
+
           if (board.length > 4) {
             isXwinner();
           }
           //just realized player 1 goes first and last, player 2 has it rough in tictactoe
           if (board.length == 9 && winner === null) {
-            whosTurn.innerText = "It's a DRAW!";
-            whosTurn.style.color = 'yellow';
+            winner = 'draw';
+            whoseTurn.innerText = "It's a DRAW!";
+            whoseTurn.style.color = 'yellow';
             pvpButton.style.display = 'none';
           }
           //player 2 turn
         } else if (
-          box.innerText === '' &&
+          boardBox[i].innerText === '' &&
           player2.turn === true &&
           winner === null
         ) {
-          whosTurn.innerText = 'Player 1 (X), make your move';
-          box.innerText = player2.marker;
+          whoseTurn.innerText = 'Player 1 (X), make your move';
+          boardBox[i].innerText = player2.marker;
           board.push(player2.marker);
-
+          boardBox[i].removeEventListener('click', test);
           player2.turn = false;
           player1.turn = true;
           if (board.length > 4) {
             isOwinner();
           }
         }
-      });
-    });
+      };
+    }
+  }
+
+  const playerMark = function () {
+    for (let i = 0; i < boardBox.length; i++) {
+      boardBox[i].addEventListener('click', boxFunctions[i]);
+    }
   };
 
-  //allow user to choose human or AI opponent
   const resetButton = document.querySelector('.reset');
   const pvpButton = document.querySelector('.pvp');
   const aiButton = document.querySelector('.ai');
 
-  const chooseMode = (function () {
-    const buttons = document.querySelectorAll('button');
-    // whosTurn.innerText = 'Choose your mode';
-    aiButton.disabled = false;
-    pvpButton.disabled = false;
+  pvpButton.addEventListener('click', runPVP);
+  aiButton.addEventListener('click', runAI);
+
+  const buttons = document.querySelectorAll('button');
+  resetButton.disabled = true;
+
+  // ----------------------------RUN PVP LOGIC--------------------------------------- //
+  whoseTurn.innerText = 'Choose a mode';
+  function runPVP() {
+    PVPLogic();
+    playerMark();
+    boardContainer.classList.remove('deactivate');
+    whoseTurn.innerText = 'Player 1 (X), make your move';
+    hideButtons();
+  }
+
+  // ----------------------------RUN AI LOGIC--------------------------------------- //
+  function runAI() {
+    AILogic();
+    AImode();
+    boardContainer.classList.remove('deactivate');
+    whoseTurn.innerText = '';
+    hideButtons();
+  }
+
+  // ----------------------------HIDE BUTTONS LOGIC--------------------------------------- //
+  function hideButtons() {
+    pvpButton.disabled = true;
+    aiButton.disabled = true;
+    pvpButton.style.display = 'none';
+    aiButton.style.display = 'none';
     resetButton.disabled = true;
+  }
 
-    buttons.forEach((button) => {
-      button.addEventListener('click', () => {
-        // if ((button.innerText = 'PVP')) {
-        //   console.log('why');
-        //   boardContainer.classList.remove('deactivate');
-        //   playerMark();
-        //   whosTurn.innerText = 'Player 1 (X), make your move';
-        //   pvpButton.disabled = true;
-        //   aiButton.disabled = true;
-        // } else
-        if (button.innerText === 'AI') {
-          AImode();
-          console.log('hello?');
-          aiButton.disabled = true;
-          pvpButton.disabled = true;
-        }
-        pvpButton.style.display = 'none';
-        aiButton.style.display = 'none';
-      });
-    });
-  })();
-
+  //each possible winning combination
   const winningConditions = [
     [boardBox[0], boardBox[1], boardBox[2]],
     [boardBox[3], boardBox[4], boardBox[5]],
@@ -103,48 +126,55 @@ const gameBoard = (() => {
     [boardBox[2], boardBox[4], boardBox[6]],
   ];
 
+  // ----------------------------WINNER LOGIC--------------------------------------- //
   //could've use factory func here too
   function isX(item) {
-    let answer = item.innerText === 'X';
-    return answer;
+    return item.innerText === 'X';
   }
 
   function isO(item) {
-    let answer = item.innerText === 'O';
-    return answer;
+    return item.innerText === 'O';
   }
 
   //checks if any of the winning conditons have all Xs
   const isXwinner = function () {
     winningConditions.forEach((condition) => {
       if (condition.every(isX)) {
-        console.log('X has won', condition);
         condition.forEach((letter) => {
           letter.style.color = '#7fff00';
         });
-
         winner = true;
-        whosTurn.innerText = 'Player 1 (X) has WON!';
+        gameOver = true;
+        initialize();
+        whoseTurn.innerText = 'Player 1 (X) has WON!';
+        boardBox.forEach((box) => {
+          if (box.innerText === 'O') box.style.color = 'red';
+        });
       }
     });
-    resetButton.disabled = false;
+    if (winner === true || board.length > 8) resetButton.disabled = false;
   };
 
   //checks if any of the winning conditons have all Os
   const isOwinner = function () {
     winningConditions.forEach((condition) => {
       if (condition.every(isO)) {
-        console.log('O has won');
         winner = true;
-        whosTurn.innerText = 'Player 2 (O) has WON!';
+        whoseTurn.innerText = 'Player 2 (O) has WON!';
+        gameOver = true;
+        initialize();
         condition.forEach((letter) => {
           letter.style.color = '#7fff00';
+          boardBox.forEach((box) => {
+            if (box.innerText === 'X') box.style.color = 'red';
+          });
         });
       }
     });
-    resetButton.disabled = false;
+    if (winner === true || board.length > 8) resetButton.disabled = false;
   };
 
+  // ----------------------------RESET LOGIC--------------------------------------- //
   //resetting letter styling back to normal from green(win)/yellow(draw)
   function isResetting() {
     winningConditions.forEach((condition) => {
@@ -152,91 +182,111 @@ const gameBoard = (() => {
         letter.style.color = '#fff';
       });
     });
+    whoseTurn.innerText = 'choose a mode';
+    whoseTurn.style.color = '#b0c6dd';
     resetButton.innerText = 'RESET';
-    whosTurn.style.color = '#fff';
     boardContainer.classList.add('deactivate');
   }
 
+  //clears board content and resets game logic
   function boardReset() {
-    //clear board array
-    board.length = 0;
-    let i = 0;
+    board.length = 0; //clear array
     boardBox.forEach((box) => {
       box.innerText = '';
-      i++;
     });
 
-    console.log(board);
     player1.turn = true;
     player2.turn = false;
+    AI.turn = false;
     winner = null;
 
     resetButton.disabled = true;
     pvpButton.disabled = false;
-    aiButton.disabled = true;
+    aiButton.disabled = false;
 
     pvpButton.style.display = 'inline-block';
     aiButton.style.display = 'inline-block';
-    whosTurn.innerText = 'Choose your mode';
 
     isResetting();
   }
 
+  //removes eventListeners from empty boxes when game is over
+  function initialize() {
+    for (let i = 0; i < boardBox.length; i++) {
+      boardBox[i].removeEventListener('click', boxFunctions[i]);
+    }
+    boxFunctions.length = 0;
+  }
+
   resetButton.addEventListener('click', boardReset);
 
-  function AImode() {
-    boardBox.forEach((box) => {
-      box.addEventListener('click', () => {
+  // ----------------------------AI LOGIC--------------------------------------- //
+
+  function AILogic() {
+    for (let i = 0; i < boardBox.length; i++) {
+      boxFunctions[i] = function PlayerVsAI() {
         //player 1 turn
-        if (box.innerText === '' && player1.turn === true && winner === null) {
-          box.innerText = player1.marker;
+        if (
+          boardBox[i].innerText === '' &&
+          player1.turn === true &&
+          winner === null
+        ) {
+          boardBox[i].innerText = player1.marker;
           board.push(player1.marker);
           player1.turn = false;
           AI.turn = true;
 
-          aiFindBox();
+          isXwinner();
+          delayMoves();
+          isOwinner();
 
           board.push(AI.marker);
           AI.turn = false;
-          player1.turn = true;
-          if (board.length > 4) {
-            isXwinner();
-          }
-          if (board.length == 9 && winner === null) {
-            whosTurn.innerText = "It's a DRAW!";
-            whosTurn.style.color = 'yellow';
+          if (board.length > 8 && winner === null) {
+            whoseTurn.innerText = "It's a DRAW!";
+            whoseTurn.style.color = 'yellow';
             pvpButton.style.display = 'none';
           }
-          //AI turn
-          // } else if (
-          //   box.innerText === '' &&
-          //   AI.turn === true &&
-          //   winner === null
-          // ) {
-          //   aiFindBox();
-          //   board.push(AI.marker);
-          //   AI.turn = false;
-          //   player1.turn = true;
         }
-      });
-    });
+      };
+    }
   }
 
+  function AImode() {
+    for (let i = 0; i < boardBox.length; i++) {
+      boardBox[i].addEventListener('click', boxFunctions[i]);
+    }
+  }
+
+  function delayMoves() {
+    if (winner === null) {
+      setTimeout(() => {
+        aiFindBox();
+      }, 400);
+    }
+
+    setTimeout(() => {
+      isOwinner();
+    }, 400);
+
+    setTimeout(() => {
+      player1.turn = true;
+    }, 600);
+  }
+
+  //makes AI find random empty box and insert its marker
   function aiFindBox() {
-    let box = Math.floor(Math.random() * 9);
-    let newBox = [];
-    if (boardBox[box].innerText === '') {
-      return (boardBox[box].innerText = AI.marker);
+    let random = Math.floor(Math.random() * 9);
+    if (boardBox[random].innerText === '') {
+      return (boardBox[random].innerText = AI.marker);
     } else {
       for (let i = 0; i < boardBox.length; i++) {
         if (boardBox[i].innerText === '') {
-          newBox.push(boardBox[i]);
+          if (board.length <= 8) {
+            return (boardBox[i].innerText = AI.marker);
+          }
         }
       }
-    }
-    console.log(newBox);
-    if (board.length <= 8) {
-      return (newBox[0].innerText = AI.marker);
     }
   }
 })();
